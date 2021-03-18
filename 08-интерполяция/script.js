e156e8b8387fabe49240e99d83db160aebbcf8fe
Odinokun <odinokun@gmail.com>
2021-03-18 12:19:55 +0200

    did lesson 8

---
+++ 08-интерполяция/script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const category = 'toys';
+
+console.log('https://someurl.com/' + category + '/' + '122');
+console.log(`https://someurl.com/${category}/122`);
+
+const user = prompt('What is your name?', '');
+alert(`Hello, ${user}`);
